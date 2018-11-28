ebca004d61f0e26a6320617a06a976f43758cf30
AK <khanadi@uw.edu>
2018-11-28 17:45:16 +0100

    Update 02_generate_correlation.R

--- 02_generate_correlation.R
+++ 02_generate_correlation.R
@@ -38,8 +38,6 @@ cors[,14]
 #Show correlation plot
 corrplot(cors)
 
-#testing contributor modifcation
-
 #==============================================================
 # ALL 5 LABELS DATA 
 #==============================================================
